236c11b001fab899128779e9239629ecdd62a368
Abdul Mannan <abdulmannang55@gmail.com>
2021-09-27 22:20:07 +0500

    working

---
+++ src/Components/videoList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const VideoList =({videos})=> {
+    return(
+        <>
+        <div>
+            
+        </div>
+         </>
+    )
+}
+export default VideoList;
