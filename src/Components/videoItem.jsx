044fb9cf8d3b284d7040ddcd9bd9dc6b02528eb3
Abdul Mannan <abdulmannang55@gmail.com>
2021-09-28 22:06:41 +0500

    youtube

---
+++ src/Components/videoItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const videoItem =({video})=>{
+    return(
+    <>
+    <div className="col-md-12">
+        <div className="row border p-2" style={{cursor: "pointer"}}>
+            <div className="col-md-3">
+                <img 
+                height="50px"
+                width= "60px"
+                alt={video.snippet.tittle}
+                src={video.snippet.thumbnails.medium.url}
+                />
+
+            </div>
+            <div className="col-md-9">
+                    <p>{video.snippet.title}</p>
+            </div>
+        </div>
+    </div>
+    </>
+    )
+
+}
+export default videoItem;
--- src/Components/videoList.jsx
+++ src/Components/videoList.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import VideoItem from "./videoItem";
 
 
 const VideoList =({videos})=> {
+
+    const renderList= videos.map(video=> { 
+        return <VideoItem video={video}/>;
+
+    });
     return(
         <>
         <div>
-            
+            {renderList}
         </div>
          </>
-    )
-}
+    );
+};
 export default VideoList;
